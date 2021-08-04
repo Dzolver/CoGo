37789d1c297fa6a0fb418413262242e3683bcfab
Byung <kaitoano0@gmail.com>
2021-08-04 15:17:59 +0900

    handle login packets

--- testCoServer.go
+++ testCoServer.go
@@ -22,7 +22,7 @@ var count = 0
 var processLimitChan = make(chan int, 1000)
 var wg sync.WaitGroup
 
-func handleTCPConnection(clientConnection net.Conn) {
+func handleTCPConnection(clientConnection net.Conn, cxt context.Context, mongoClient *mongo.Client) {
 	fmt.Print(".")
 	for {
 		netData, err := bufio.NewReader(clientConnection).ReadString('\n')
@@ -30,7 +30,25 @@ func handleTCPConnection(clientConnection net.Conn) {
 			fmt.Println(err)
 			return
 		}
-		rcvMessage := strings.TrimSpace(string(netData))
+		data := strings.TrimSpace(string(netData))
+		packetCode := data[0:2]
+		rcvMessage := strings.Replace(data, packetCode, "", -1)
+
+		if packetCode == "L#" {
+			fmt.Println("Login packet received!")
+			username := strings.Split(rcvMessage, "?")[0]
+			password := strings.Split(rcvMessage, "?")[1]
+			handleLogin(username, password, mongoClient)
+		}
+		if packetCode == "R#" {
+			fmt.Println("Register packet received!")
+			username := strings.Split(rcvMessage, "?")[0]
+			password := strings.Split(rcvMessage, "?")[1]
+			//THIS NEEDS TO BE REDIRECTED TO A REGISTER FUNCTION
+			handleLogin(username, password, mongoClient)
+		}
+
+		fmt.Println("Received message from client : ", rcvMessage)
 		if rcvMessage == "STOP" {
 			fmt.Println("Client connection has exited")
 			count--
@@ -42,7 +60,7 @@ func handleTCPConnection(clientConnection net.Conn) {
 	clientConnection.Close()
 }
 
-func tcpListener(PORT string) {
+func tcpListener(PORT string, cxt context.Context, mongoClient *mongo.Client) {
 	listenerConnection, err := net.Listen("tcp", PORT)
 	if err != nil {
 		fmt.Println(err)
@@ -55,7 +73,7 @@ func tcpListener(PORT string) {
 			fmt.Println(err)
 			return
 		}
-		go handleTCPConnection(clientConnection)
+		go handleTCPConnection(clientConnection, cxt, mongoClient)
 		count++
 	}
 }
@@ -97,25 +115,101 @@ func udpListener(PORT string) {
 	}
 }
 
-func findMongoDataExample(context context.Context, mongoClient *mongo.Client) {
-	if err := mongoClient.Ping(context, readpref.Primary()); err != nil {
+func findMongoDataExample(cxt context.Context, mongoClient *mongo.Client) {
+	if err := mongoClient.Ping(cxt, readpref.Primary()); err != nil {
 		panic(err)
 	}
 	fmt.Println("Mongo DB client has been pinged successfully!")
 	database := mongoClient.Database("test")
 	testCollection := database.Collection("test")
-	filterCursor, err := testCollection.Find(context, bson.M{"score": 500})
+	filterCursor, err := testCollection.Find(cxt, bson.M{"score": 500})
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	var filterResult []bson.M
-	if err = filterCursor.All(context, &filterResult); err != nil {
+	if err = filterCursor.All(cxt, &filterResult); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Data found from DB: ", filterResult[0]["name"], " scored ", filterResult[0]["score"], " points!")
 }
-
+func handleLogin(username string, password string, mongoClient *mongo.Client) {
+	if !lookForUser(username, mongoClient) {
+		createUser(username, password, mongoClient)
+		if validateUser(username, password, mongoClient) {
+			fmt.Println("Login successful!")
+		} else {
+			fmt.Println("Login failed!")
+		}
+	} else {
+		if validateUser(username, password, mongoClient) {
+			fmt.Println("Login successful!")
+		} else {
+			fmt.Println("Login failed!")
+		}
+	}
+}
+func createUser(username string, password string, mongoClient *mongo.Client) {
+	cxt, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	database := mongoClient.Database("player")
+	users := database.Collection("users")
+	createResult, err := users.InsertOne(cxt, bson.D{
+		{"username", username},
+		{"password", password},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("New user added to db : ", createResult.InsertedID)
+}
+func lookForUser(username string, mongoClient *mongo.Client) bool {
+	cxt, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	database := mongoClient.Database("player")
+	users := database.Collection("users")
+	filterCursor, err := users.Find(cxt, bson.M{"username": username})
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	var filterResult []bson.M
+	if err = filterCursor.All(cxt, &filterResult); err != nil {
+		log.Fatal(err)
+	}
+	if len(filterResult) == 0 {
+		//no users found
+		return false
+	} else if len(filterResult) == 1 {
+		return true
+	}
+	return true
+}
+func validateUser(username string, password string, mongoClient *mongo.Client) bool {
+	cxt, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := mongoClient.Ping(cxt, readpref.Primary()); err != nil {
+		panic(err)
+	}
+	database := mongoClient.Database("player")
+	users := database.Collection("users")
+	filterCursor, err := users.Find(cxt, bson.M{"username": username, "password": password})
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	var filterResult []bson.M
+	if err = filterCursor.All(cxt, &filterResult); err != nil {
+		log.Fatal(err)
+	}
+	if len(filterResult) == 0 {
+		//no users found
+		return false
+	} else if len(filterResult) == 1 {
+		return true
+	}
+	return true
+}
 func main() {
 	arguments := os.Args
 	if len(arguments) == 1 {
@@ -126,28 +220,28 @@ func main() {
 	uri := "mongodb+srv://admin:london1234@cluster0.8acnf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 	//initialize mongoDB client
-	context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	cxt, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	mongoClient, err := mongo.Connect(context, options.Client().ApplyURI(uri))
+	mongoClient, err := mongo.Connect(cxt, options.Client().ApplyURI(uri))
 	if err != nil {
 		panic(err)
 	}
 
 	//disconnect mongoDB client on return
 	defer func() {
-		if err = mongoClient.Disconnect(context); err != nil {
+		if err = mongoClient.Disconnect(cxt); err != nil {
 			panic(err)
 		}
 	}()
 	//sample mongoDB query
-	findMongoDataExample(context, mongoClient)
+	findMongoDataExample(cxt, mongoClient)
 
 	//PORT = ":20001"
 	PORT := ":" + arguments[1]
 
 	// add to sync.WaitGroup
 	wg.Add(1)
-	go tcpListener(PORT)
+	go tcpListener(PORT, cxt, mongoClient)
 	wg.Add(1)
 	go udpListener(PORT)
 	wg.Wait()
